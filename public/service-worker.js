41567547c3d2cd6c1cf0c04c8d205ef1df9af05c
Pirooz Wallace <PiroozWallace@outlook.com>
2021-02-27 11:12:38 -0800

    changed dimensions of icons and then redid manifest

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,14 +2,15 @@ const FILES_TO_CACHE = ["/", "/index.html",
 "/index.js", 
 "/styles.css",
 "/db.js",
-"icons/icon-72x72.png",
-"icons/icon-96x96.png",
-"icons/icon-128x128.png",
-"icons/icon-144x144.png",
-"icons/icon-152x152.png",
-"icons/icon-192x192.png",
-"icons/icon-384x384.png",
-"icons/icon-512x512.png",];
+"images/icons/icon-72x72.png",
+"images/icons/icon-96x96.png",
+"images/icons/icon-128x128.png",
+"images/icons/icon-144x144.png",
+"images/icons/icon-152x152.png",
+"images/icons/icon-192x192.png",
+"images/icons/icon-384x384.png",
+"images/icons/icon-512x512.png",
+"service-worker.js"];
 
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
